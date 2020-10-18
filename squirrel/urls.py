3f7e1da4a40ffaec551ecd2fe8c4af335e26d7b8
sophiazhouzwx <73026204+sophiazhouzwx@users.noreply.github.com>
2020-10-17 20:03:45 -0400

    add map

--- squirrel/urls.py
+++ squirrel/urls.py
@@ -6,7 +6,7 @@ app_name = 'squirrel'
 
 urlpatterns = [ 
     path('', views.index),
-    #path('map/', include('maps.urls')), 
+    path('map/', views.map, name='map'), 
     path('sighting/', views.sighting),
     #path('<int:Unique_squirrel_id>/', views.get_squirrel),
     #path('sighting/stats/', views.general_stats), 
