6f97f29e448ae832becde743f5c3334920d1cbf2
Jennifer Zhang <yz3932@columbia.edu>
2020-10-22 02:58:05 +0000

    Wrap up the homepage html template

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/wz2551/squirrel-project/env"
+set -gx VIRTUAL_ENV "/home/yz3932/proj/squirrel-project/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
